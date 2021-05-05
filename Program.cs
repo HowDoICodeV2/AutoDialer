7bef38272d527346dcf82ad3bd41c9fa5f20cfb1
aiden lundeen <aiden.lundeen@student.centralia.edu>
2021-05-05 09:58:30 -0700

    Fix broken Main Code, add initial data to main

--- Program.cs
+++ Program.cs
@@ -8,13 +8,33 @@ namespace AutoDialer
 
         static void Main(string[] args)
         { // empty space is to prevent merge conflicts on the same line
+            Phone[] phoneList = new Phone[10];
+            phoneList[0] = new HomePhone("CompuTest", "(303) 985-5060", "1");
+            phoneList[1] = new CellPhone("Curtis Manufacturing", "(603) 532-4123", "2");
+            phoneList[2] = new HomePhone("Data Functions", "(800) 876-2524", "1");
+            phoneList[3] = new HomePhone("Donnay Repair", "(708) 397-3330", "1");
+            phoneList[4] = new HomePhone("ErgoNomic Inc", "(360) 434-3894", "1");
+            phoneList[5] = new HomePhone("ErgoSource", "(800) 969-4374", "1");
+            phoneList[6] = new CellPhone("Fox Bay Industries", "(800) 874-8527", "2");
+            phoneList[7] = new CellPhone("Glare-Guard", "(800) 545-6254", "2");
+            phoneList[8] = new CellPhone("Hazard Comm Specialists","(407)783-6641", "2");
+            phoneList[9] = new CellPhone("Komfort Support", "(714) 472-4409","2");
+           
             
-            // Array phoneList[10] = new(new Phone(phoneNumber)); // this 10 is here to max the array size, I don't believe arrays are dynamic in C#, currently commented for a different implementation
-            Phone phoneList[10] = Initialize(); // I am not sure what the difference between these implementations is. I simply know this is the humble array in C++, this has no data nor empty phones in it since they will have to be made as their specific classes later
+           
+            
+            
+            
+            
+            
+            
 
 
 
 
+
+
+            
 
 
 
@@ -27,19 +47,9 @@ namespace AutoDialer
 
 
         }
-        // add the initial data, I am looking for a function so we know the size this will take within main
-        public Phone[10] Initialize ()
-        {
-            /* CompuTest, (303) 985-5060, 1
-            * Curtis Manufacturing, (603) 532-4123, 2
-            * DataFuntions, (800) 876-2524, 1
-            * Donnay Repair, (708) 397-3330, 1
-            * ErgoNomic Inc, (360) 434-3894, 1
-            * Fox Bay Industries, (800) 874-8527, 2
-            * Glare-Guard, (800) 545-6254, 2
-            * Hazard COmm Specialist, (407) 783-6641, 2
-            * Komfort Support, (714) 472-4409, 2
-            */
+
+
+            
             
 
 
@@ -48,7 +58,7 @@ namespace AutoDialer
 
 
         }
-        // User input section, ensure that the user can not exceed 10 phones in the array. My guess is only allow one phone, since we have 9 initial data
+        // User input section
 
 
 
@@ -68,7 +78,7 @@ namespace AutoDialer
 
 
 
-        // string interpereter, to take initial data and divide it into the three separate variables without commas.
+
 
 
 

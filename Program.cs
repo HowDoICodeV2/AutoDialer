8a937d14b5eb8e405d9638be2a8c8c725f660ff9
Mitch Partee <mitch.partee@student.centralia.edu>
2021-05-12 08:44:16 -0700

    Changed UI part

--- Program.cs
+++ Program.cs
@@ -30,27 +30,36 @@ namespace AutoDialer
 
 
             string companyName;
-            string areaCode;
-            string phoneNumber;
             string numberType;
             string fullNumber;
+
+            ConsoleKeyInfo keypress = Console.ReadKey();
             Console.WriteLine("Welcome to the Auto Dialer program");
             Console.WriteLine("Please enter the name of the business you wish to dial: ");
             companyName = Console.ReadLine();
-            Console.WriteLine("Please enter the area code for the number you wish to dial: ");
-            areaCode = Console.ReadLine();
-            Console.WriteLine("Please enter the number you wish to dial like this 'XXX-XXXX': ");
-            phoneNumber = Console.ReadLine();
-            Console.WriteLine("Is this number a Land Line (1) or a Cell Phone (2): ");
-            numberType = Console.ReadLine();
-            fullNumber = "(" + areaCode + ") " + phoneNumber;
-            if (numberType.Equals("1"))
+            Console.WriteLine("Please enter the number you wish to dial: ");
+            fullNumber = Console.ReadLine();
+            while (keypress.Key != ConsoleKey.D1 || keypress.Key != ConsoleKey.D2)
             {
-                phoneList[0] = new HomePhone(companyName, fullNumber, numberType);
-            }
-            else
-            {
-                phoneList[0] = new CellPhone(companyName, fullNumber, numberType);
+                Console.WriteLine("Is this number a Land Line (1) or a Cell Phone (2): ");
+                keypress = Console.ReadKey();
+
+                numberType = Console.ReadLine();
+                if (keypress.Key == ConsoleKey.D1)
+                {
+                    phoneList[0] = new HomePhone(companyName, fullNumber, numberType);
+                    break;
+                }
+                else if (keypress.Key == ConsoleKey.D2)
+                {
+                    phoneList[0] = new CellPhone(companyName, fullNumber, numberType);
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Hey... There are two options here... pick (1) for Land Line or (2) for Cell Phone.");
+                }
             }
             
 
